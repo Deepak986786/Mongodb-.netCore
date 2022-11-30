a8ee608fd13d24df830854cad4985ae91fe86d65
Deepak <57728553+Deepak986786@users.noreply.github.com>
2022-11-30 14:41:06 +0530

    first commit

--- MongoRepository/Models/User.cs
+++ MongoRepository/Models/User.cs
@@ -6,7 +6,6 @@ namespace MongoRepository.Models
     public class User
     {
         [BsonId]
-        
         public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
